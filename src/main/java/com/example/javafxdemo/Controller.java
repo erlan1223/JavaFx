2d0905ebb32f6451bcf219f65b0c62b1cd24f52a
erlan1223 <erlan_23.12@mail.ru>
2024-05-30 20:46:02 +0500

    Домашка по JavaFX Yerlan Osserbay

--- src/main/java/com/example/javafxdemo/Controller.java
+++ src/main/java/com/example/javafxdemo/Controller.java
@@ -1,14 +1,12 @@
 package com.example.javafxdemo;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -16,38 +14,83 @@ public class Controller implements Initializable {
     private TextArea questionArea;
 
     @FXML
-    RadioButton answer1, answer2, answer3, answer4;
+    private CheckBox answer1, answer2, answer3, answer4;
 
     @FXML
-    ToggleGroup answers;
-    private String question = "Ваш любимый фрукт?";
+    private Button nextButton, submitButton;
 
-    private String[] variants = {
-            "Банан", "Киви", "Апельсин", "Мандарин"
-    };
+    private final List<String> questions = Arrays.asList(
+            "Какие из следующих стран находятся в Азии?",
+            "Какие из этих людей были космонавтами Казахстана?",
+            "Какие из следующих животных обитают в Южной Америке?",
+            "Как называется столица Австралии?",
+            "Какое количество планет в Солнечной системе?"
+    );
 
-    private String correctVariant = "Апельсин";
+    private final List<String[]> answerOptions = Arrays.asList(
+            new String[]{"Суринаи", "Того", "Тайланд", "Камбоджа"},
+            new String[]{"Аубакиров", "Шоканов", "Мусабаев", "Райымбеков"},
+            new String[]{"Кенгуру", "Броненосец", "Капибара", "Коала"},
+            new String[]{"Виктория", "Сидней", "Канберра","Мельбурн"},
+            new String[]{"8", "9", "10","11"}
+    );
 
+    private final List<Set<String>> correctAnswers = Arrays.asList(
+            new HashSet<>(Arrays.asList("Тайланд", "Камбоджа")),
+            new HashSet<>(Arrays.asList("Аубакиров", "Мусабаев")),
+            new HashSet<>(Arrays.asList("Броненосец", "Капибара")),
+            new HashSet<>(List.of("Канберра")),
+            new HashSet<>(List.of("8"))
+    );
+
+    private int currentQuestionIndex = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println();
-        System.out.println(1);
-        System.out.println(2);
-        questionArea.setText(question);
-        answer1.setText(variants[0]);
-        answer2.setText(variants[1]);
-        answer3.setText(variants[2]);
-        answer4.setText(variants[3]);
+        loadQuestion(currentQuestionIndex);
+        nextButton.setOnAction(this::nextQuestion);
+        submitButton.setOnAction(this::tryToAnswer);
     }
 
-    public void tryToAnswer() {
-        if(((RadioButton)answers.getSelectedToggle()).getText().equals(correctVariant)) {
+    private void loadQuestion(int index) {
+        questionArea.setText(questions.get(index));
+        String[] answers = answerOptions.get(index);
+        answer1.setText(answers[0]);
+        answer2.setText(answers[1]);
+        answer3.setText(answers[2]);
+        answer4.setText(answers[3]);
+        answer1.setSelected(false);
+        answer2.setSelected(false);
+        answer3.setSelected(false);
+        answer4.setSelected(false);
+    }
+
+    @FXML
+    private void tryToAnswer(ActionEvent event) {
+        Set<String> selectedAnswers = new HashSet<>();
+        if (answer1.isSelected()) selectedAnswers.add(answer1.getText());
+        if (answer2.isSelected()) selectedAnswers.add(answer2.getText());
+        if (answer3.isSelected()) selectedAnswers.add(answer3.getText());
+        if (answer4.isSelected()) selectedAnswers.add(answer4.getText());
+
+        if (selectedAnswers.equals(correctAnswers.get(currentQuestionIndex))) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Правильно!");
             alert.showAndWait();
         } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Не правильно!");
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Неправильно!");
             alert.showAndWait();
         }
     }
+
+    @FXML
+    private void nextQuestion(ActionEvent event) {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.size()) {
+            loadQuestion(currentQuestionIndex);
+        } else {
+            currentQuestionIndex = 0;
+            loadQuestion(currentQuestionIndex);
+        }
+    }
 }
+
--- src/main/java/com/example/javafxdemo/HelloApplication.java
+++ src/main/java/com/example/javafxdemo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 300);
-        stage.setTitle("T-system");
+        stage.setTitle("Викторина");
         stage.setScene(scene);
         stage.show();
     }
